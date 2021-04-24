37c4a9f5ef0eb24ff8e303bab81a6678cad166a4
V-The Windows Ninja <vinniehaz16@gmail.com>
2021-04-25 00:43:15 +0300

    Aspirant selection & Vote submission mods

--- app/src/main/java/com/android/jkura/VoteSubmissionActivity.java
+++ app/src/main/java/com/android/jkura/VoteSubmissionActivity.java
@@ -1,14 +1,60 @@
 package com.android.jkura;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.google.android.material.button.MaterialButton;
+
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class VoteSubmissionActivity extends AppCompatActivity {
+public class VoteSubmissionActivity extends AppCompatActivity implements View.OnClickListener {
+    //Popup
+    ImageView popAspImage;
+    TextView popAspName,popAspPosition;
+    MaterialButton popCancelBtn,popAcceptBtn;
 
+    MaterialButton submitVote;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vote_submission);
+
+        submitVote = findViewById(R.id.submitVoteBtn);
+
+        submitVote.setOnClickListener(this);
+    }
+
+    private void confirmVotePopUp(){
+        ViewGroup viewGroup = findViewById(android.R.id.content);
+        View dialogView = LayoutInflater.from(this).inflate(R.layout.confirm_vote_popup, viewGroup, false);
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setView(dialogView)
+                .setCancelable(false);
+        final AlertDialog confirmVoteDialog = builder.create();
+        confirmVoteDialog.show();
+
+        popAspImage = dialogView.findViewById(R.id.popAspirantImage);
+        popAspName = dialogView.findViewById(R.id.popAspirantName);
+        popAspPosition = dialogView.findViewById(R.id.popAspirantPosition);
+        popCancelBtn = dialogView.findViewById(R.id.popCancelBtn);
+        popAcceptBtn = dialogView.findViewById(R.id.popOkBtn);
+
+        popCancelBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                confirmVoteDialog.dismiss();
+            }
+        });
+    }
+    @Override
+    public void onClick(View v) {
+        if (v.equals(submitVote)){
+            confirmVotePopUp();
+        }
     }
 }
