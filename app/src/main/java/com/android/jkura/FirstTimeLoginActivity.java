d811faff01dfa718d3719336210c49a58d424874
Maina Josephat <josendungu.jm@gmail.com>
2021-04-26 13:19:36 +0300

    Erick's commits and some changes

---
+++ app/src/main/java/com/android/jkura/FirstTimeLoginActivity.java
@@ -0,0 +1,14 @@
+package com.android.jkura;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FirstTimeLoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first_time_login);
+    }
+}
