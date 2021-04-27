65f7fa5724fcc20829385f6b1f92d3f8df46e766
Maina Josephat <josendungu.jm@gmail.com>
2021-04-27 19:04:51 +0300

    Added the pie chart in Home Activity

--- app/src/main/java/com/android/jkura/HomeActivity.java
+++ app/src/main/java/com/android/jkura/HomeActivity.java
@@ -4,11 +4,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.razerdp.widget.animatedpieview.AnimatedPieView;
+import com.razerdp.widget.animatedpieview.AnimatedPieViewConfig;
+import com.razerdp.widget.animatedpieview.data.SimplePieInfo;
+
 public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        AnimatedPieView animatedPieView = findViewById(R.id.pie_chart);
+        AnimatedPieViewConfig config = new AnimatedPieViewConfig();
+        config.startAngle(-90)// Starting angle offset
+                .addData(new SimplePieInfo(30, getResources().getColor(R.color.pieBlue), "Number 1"))//Data (bean that implements the IPieInfo interface)
+                .addData(new SimplePieInfo(18, getResources().getColor(R.color.pieYellow), "Number 2"))
+                .addData(new SimplePieInfo(35, getResources().getColor(R.color.pieOthers), "Others"))
+                .duration(2000)
+        .drawText(true)
+        .textSize(30);// draw pie animation duration
+
+        animatedPieView.applyConfig(config);
+        animatedPieView.start();
     }
 }
--- app/src/main/java/com/android/jkura/SplashScreenActivity.java
+++ app/src/main/java/com/android/jkura/SplashScreenActivity.java
@@ -2,7 +2,9 @@ package com.android.jkura;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
@@ -10,5 +12,7 @@ public class SplashScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+       startActivity(new Intent(this, HomeActivity.class));
     }
 }
